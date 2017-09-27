e722f960e4abf1aab830db5ca18348919339e7ee
yanjian_zhang <yanjian_zhang@suishouji.com>
2017-09-27 09:51:13 +0800

    scss

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,41 @@
 // http://eslint.org/docs/user-guide/configuring
-
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
-  env: {
-    browser: true,
-  },
-  extends: 'airbnb-base',
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  // check if imports actually resolve
-  'settings': {
-    'import/resolver': {
-      'webpack': {
-        'config': 'build/webpack.base.conf.js'
-      }
+  'root': true,
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'sourceType': 'module',
+    'extends': 'standard',
+    'ecmaFeatures': {
+      'experimentalObjectRestSpread': true,
+      'jsx': true
     }
   },
-  // add your custom rules here
+
+  'env': {
+    'browser': true,
+    'node': true
+  },
+
+  'plugins': [
+   'html'
+  ],
+
+ /**
+  * add your custom rules here
+  * "off" -> 0
+  * "warn" -> 1
+  * "error" -> 2
+  */
   'rules': {
-    // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      'js': 'never',
-      'vue': 'never'
-    }],
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      'optionalDependencies': ['test/unit/index.js']
-    }],
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'array-bracket-spacing': [2, "always"],
+    "comma-dangle": [2, "never"],
+    "comma-spacing": [2, { "before": false, "after": true }],
+    "eqeqeq": [2, "allow-null"],
+    // 要求在数组括号内使用一个或多个空格、或折行
+    "array-bracket-spacing": "always"
   }
 }
